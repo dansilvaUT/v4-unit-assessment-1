3d9d7ee3d1469b496c76502d45c23c64533c3c0c
dansilvaUT <dan13silv@gmail.com>
2020-12-04 10:41:46 -0800

    JS 2 completed

--- javascript-2/practice-js-2.js
+++ javascript-2/practice-js-2.js
@@ -10,7 +10,7 @@
 */
 
 //CODE HERE
-
+let gitDefinition = 'Git is a version control system that allows you to track, commit and push changes to your code repo';
 //////////////////PROBLEM 2////////////////////
 /*
     Create a variable called 'gitHubDefinition'.  
@@ -18,7 +18,7 @@
 */
 
 //CODE HERE
-
+let gitHubDefinition = 'GitHub is a cloud software that allows you to store your code repositories and use git to manage them';
 //////////////////PROBLEMS 3 - 9////////////////////
 /*
     For the next several problems you will be creating objects containing information about different git 
@@ -34,7 +34,10 @@
 */
 
 //CODE HERE
-
+const init = {
+    description: gitDefinition,
+    code: 'git init'
+};
 //////////////////PROBLEM 4////////////////////
 /*
     Create an object called 'clone' with 'description' and 'code' properties 
@@ -42,23 +45,32 @@
 */
 
 //CODE HERE
-
+const clone = {
+    description: 'Clones a repo to your local machine',
+    code: 'git clone'
+};
 //////////////////PROBLEM 5////////////////////
 /*
-    Create an object called 'status' with 'description' and 'code' properties 
+    Create an object called 'status' with 'description' and 'code' properties
     following the guidelines above to describe the status command.
 */
 
 //CODE HERE
-
+const status = {
+    description: 'Shows you that status of what files have had changes to them in your repo',
+    code: 'git status'
+};
 //////////////////PROBLEM 6////////////////////
 /*
-    Create an object called 'add' with 'description' and 'code' properties 
+    Create an object called 'add' with 'description' and 'code' properties
     following the guidelines above to describe the add command.
 */
 
 //CODE HERE
-
+const add = {
+    description: 'Adds the changed files to the queue to be commited',
+    code: 'git add'
+};
 //////////////////PROBLEM 7////////////////////
 /*
     Create an object called 'commit' with 'description' and 'code' properties
@@ -66,7 +78,10 @@
 */
 
 //CODE HERE
-
+const commit = {
+    description: "Commits your changes from your repo",
+    code: 'git commit -m ""'
+};
 //////////////////PROBLEM 8////////////////////
 /*
     Create an object called 'addRemote' with 'description' and 'code' properties
@@ -74,11 +89,18 @@
 */
 
 //CODE HERE
-
+const addRemote = {
+    description: "Adds a remote repo",
+    code: 'git remote add origin'
+};
 //////////////////PROBLEM 9////////////////////
 /*
-    Create an object called 'push' with 'description' and 'code' properties 
+    Create an object called 'push' with 'description' and 'code' properties
     following the guidelines above to describe the push command.
 */
 
 //CODE HERE
+const push = {
+    description: 'Pushes the local changes to your cloud repo',
+    code: 'git push'
+};
